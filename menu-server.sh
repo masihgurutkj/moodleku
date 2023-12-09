9a06219f6310ffc242a9367117f616d996146ffa
masihgurutkj <49093218+masihgurutkj@users.noreply.github.com>
2023-12-09 10:35:13 +0700

    Update menu-server.sh

--- menu-server.sh
+++ menu-server.sh
@@ -1,28 +1,30 @@
 ####################       MENU STACK - DOCKER MOODLE     ################# 
 #!/bin/bash
 clear
-#echo "+++++++++++++++++++++++++++++++++++++++++"
-echo "STACK - DOCKER MOODLE"
-echo "::::::::::::::::::"
-echo "s - Jalankan Container [Mode Listing Proses]"
-echo "------------------"
-echo "ss - Jalankan Container [Mode Senyap] "
-echo "------------------"
-echo "e - Hapus Container "
-echo "::::::::::::::::::"
-echo "1 - Aktifkan "
-echo "------------------"
-echo "0 - Non Aktifkan "
-echo "::::::::::::::::::"
-
+echo ":::::::::::::::::::::::::::::::::::::::::::::::"
+echo "::::::::::: STACK - DOCKER MOODLE :::::::::::::"
+echo ":::::::::::::::::::::::::::::::::::::::::::::::"
 echo " "
-
+echo "bl - Jalankan Container [ Mode: Build Listing ]"
+echo "-----------------------------------------------"
+echo "b - Jalankan Container [ Mode: Build ] "
+echo "-----------------------------------------------"
+echo "x - Hapus Container "
+echo "-----------------------------------------------"
+echo "1 - Start Container "
+echo "-----------------------------------------------"
+echo "0 - Stop Container"
+echo "-----------------------------------------------"
+echo " "
+echo "[ Ctrl+C ] Keluar Menu Server ini "
+echo " "
+echo ":::::::::::::::::::::::::::::::::::::::::::::::"
 read moodle;
 case $moodle in
-  s) docker-compose -p 'moodleku' up --build;;
-  s) docker-compose -p 'moodleku' up -d --build;;
-  e) docker-compose -p 'moodleku' down;;
-  1) docker-compose -p 'moodleku' start;;
+  bl) docker-compose -p 'moodleku' up --build;;
+  b) docker-compose -p 'moodleku' up -d --build && ./menu-server.sh;;
+  x) docker-compose -p 'moodleku' down && ./menu-server.sh;;
+  1) docker-compose -p 'moodleku' start && ./menu-server.sh;;
   0) docker-compose -p 'moodleku' stop;;
   *) clear && echo "Keluar dari Pilihan Service Stack Docker";;
 esac 
